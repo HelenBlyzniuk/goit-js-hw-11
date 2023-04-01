5182835c81240c8a785f6010727a25cc9ab29f91
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-04-01 15:57:56 +0300

    into async

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,11 +1,12 @@
 
-export function fetchPhoto(name) {
+import axios from 'axios';
+
+export async function fetchPhoto(name) {
     const URL = `https://pixabay.com/api/&key=34953868-e619b94b5038a72e794119bd3&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=30`;
-      return fetch(URL).then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
+      const response = await axios.get(URL);
+    if (!response.ok) {
+        throw new Error(response.status);
     }
-            return response.json()
-        })
+    return await response.json();
   }     
  
