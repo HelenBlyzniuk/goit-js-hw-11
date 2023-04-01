33c19cf73a873ce8b133f039ff5700a50ccd51c4
HelenBlyzniuk <helenblyzniuk2@gmail.com>
2023-04-01 17:25:18 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -14,15 +14,49 @@ const refs = {
 
 refs.formEl.addEventListener("submit", onFormSubmit);
 
-
+let page = 1;
 
 async function onFormSubmit(e) {
     e.preventDefault();
     let name = e.target.elements.searchQuery.value.trim();
     console.log(name);
+    if (name === '') {
+     Notiflix.Notify.failure('Enter data you want to find');   
+    }
     
-    const pictures = await fetchPhoto(name);
+    const pictures = await fetchPhoto(name, page);
+    if (pictures === []) {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');  
+    } 
     console.log(pictures);
+}
+
+function galleryMarkup(array) {
+    const markup = array.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        downloads }) => {
+        return ` <div class="photo-card">
+        <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes:${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views:${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments:${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads:${downloads}</b>
+        </p>
+      </div>
+      </div>`
+    }).join('');
 }
          
 
